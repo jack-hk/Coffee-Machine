814ec2dd4109b5413c59da54f7b219d5fc590186
Jack HK <jackhughesking@gmail.com>
2021-09-30 14:19:01 +0100

    rewrite

--- CoffeeMachine/CoffeeMachine.cpp
+++ CoffeeMachine/CoffeeMachine.cpp
@@ -1,45 +1,36 @@
-// CoffeeMachine.cpp 
 
 #include <iostream>
-#include <windows.h>
 #include <string>
+#include <windows.h>
 
 using namespace std;
 
-void customChar(string msg) {
-	std::cout << msg << std::endl;
-	Sleep(1000);
+class DrinksMenu {
+public:
+	int itemID;
+	double itemPrice;
+	string itemName;
+};
+
+void Broadcast(string msg) {
+	cout << msg;
 }
 
-void emptyChar() {
-	std::cout << std::endl;
-	Sleep(1000);
+int main() {
+
+	bool debugMode = true;
+
+	DrinksMenu Coffee;
+	Coffee.itemID = 4;
+	Coffee.itemPrice = 2.99;
+	Coffee.itemName = "Regular Coffee";
+
+	DrinksMenu Tea;
+	Tea.itemID = 1;
+	Tea.itemPrice = 5.99;
+	Tea.itemName = "Tea";
+
+	Broadcast(Tea.itemName);
+
+	return 0;
 }
-
-void pointsChar() {
-	std::cout << "..." << std::endl;
-	Sleep(1500);
-}
-
-int main()
-{
-	std::cout << "Welcome to CoffeeVirtual (10.2020)" << std::endl;
-	for (size_t i = 0; i < 4; i++)
-	{
-		if (i == 0) {
-			emptyChar();
-			std::cout << "Loading: Initialised" << std::endl;
-		}
-		else if (i == 3) {
-			std::cout << "Loading: Completed" << std::endl;
-		}
-		if (i < 3) {
-			pointsChar();
-		}
-	}
-	std::cout << "Please make your selection:" << std::endl;
-
-	
-}
-
-
