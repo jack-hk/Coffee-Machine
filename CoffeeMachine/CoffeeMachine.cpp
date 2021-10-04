496ea70ce2907ce72a5d14b480895b5ea585e1bf
Jack HK <jackhughesking@gmail.com>
2021-10-04 23:27:00 +0100

    startup + menu additions

--- CoffeeMachine/CoffeeMachine.cpp
+++ CoffeeMachine/CoffeeMachine.cpp
@@ -27,42 +27,51 @@ private:
 		else
 			return false;
 	}
-	void ColorText(int color) {
+	int ColorText(int color) {
 		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 		SetConsoleTextAttribute(hConsole, color);
+		return color;
 	}
 
 public:
 	void Message(string msg) {
 		cout << msg << endl;
 	}
-	void Divider(int color) {
-		ColorText(color);
-		cout << eclipse << endl;
-	}
 	void TimedMessage(string msg, int color, int time, bool title) {
 		ColorText(color);
+		Sleep(time / 2);
 		TitleCheck(title);
 		cout << msg;
 		TitleCheck(title);
 		cout << endl;
-		Sleep(time);
+		Sleep(time / 2);
 	}
-	void Price(string msg) {		//sample
+	void Divider(int color, int length) {
+		ColorText(color);
+		cout << " ";
+		for (size_t i = 0; i < length; i++)
+		{
+			cout << divider[1];
+		}
+		cout << " " << endl;
+	}
+	void Loader(int length, int duration) {
+		for (size_t i = 0; i < length; i++)
+		{
+			Sleep(duration / length / 2);
+			cout << eclipse << endl;
+			Sleep(duration / length / 2);
+		}
+	}
+	void Price(string msg) {
 		cout << "$" << msg << endl;
 	}
 };
 
-void MainMenu() {
-	Broadcast MainMenu;
-	MainMenu.TimedMessage("Hello World", 10, 3000, true);
-	MainMenu.TimedMessage("Hello World", 5, 3000, false);
-	MainMenu.Divider(8);
-	MainMenu.Price("5.99");
-}
+void StartUp();
+void MainMenu();
 
 int main() {
-	MainMenu();
 	Drinks Tea;
 	Tea.itemID = 1;
 	Tea.itemPrice = 5.99;
@@ -78,5 +87,28 @@ int main() {
 	HotChoc.itemPrice = 3.99;
 	HotChoc.itemName = "Hot Chocolate";
 
+	StartUp();
 	return 0;
+}
+
+void StartUp() {
+	Broadcast StartUp;
+	StartUp.Divider(10, 34);
+	StartUp.TimedMessage("Welcome to CoffeeVirtual", 10, 0, true);
+	StartUp.Divider(10, 34);
+	if (debugMode == true) {
+		StartUp.TimedMessage("Debug Enabled", 7, 0, true);
+	}
+	cout << endl;
+	StartUp.TimedMessage("Starting up", 8, 1000, false);
+	StartUp.Loader(3, 3000);
+	StartUp.TimedMessage("Completed", 8, 1000, false);
+	cout << endl;
+
+	MainMenu();
+}
+
+void MainMenu() {
+	Broadcast MainMenu;
+	MainMenu.TimedMessage("Choose your option.", 10, 0, true);
 }
