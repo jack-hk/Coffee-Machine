e83f7de3da66e62065cc69ba3931f65a2199cece
Jack HK <jackhughesking@gmail.com>
2021-10-04 12:00:34 +0100

    working on messages
    add colour, title and timer options

--- CoffeeMachine/CoffeeMachine.cpp
+++ CoffeeMachine/CoffeeMachine.cpp
@@ -5,32 +5,78 @@
 
 using namespace std;
 
-class DrinksMenu {
+bool debugMode = true; //Debug
+
+class Drinks {
 public:
-	int itemID;
-	double itemPrice;
+	int itemID = 0; //make const?
+	double itemPrice = 0;
 	string itemName;
 };
 
-void Broadcast(string msg) {
-	cout << msg;
+class Broadcast {
+private:
+	string divider = " ~~~~ ";
+	string eclipse = "...";
+
+	bool TitleCheck(bool title) {
+		if (title == true) {
+			cout << divider;
+			return true;
+		}
+		else
+			return false;
+	}
+	void ColorText(int color) {
+		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+		SetConsoleTextAttribute(hConsole, color);
+	}
+
+public:
+	void Message(string msg) {
+		cout << msg << endl;
+	}
+	void Divider(int color) {
+		ColorText(color);
+		cout << eclipse << endl;
+	}
+	void TimedMessage(string msg, int color, int time, bool title) {
+		ColorText(color);
+		TitleCheck(title);
+		cout << msg;
+		TitleCheck(title);
+		cout << endl;
+		Sleep(time);
+	}
+	void Price(string msg) {		//sample
+		cout << "$" << msg << endl;
+	}
+};
+
+void MainMenu() {
+	Broadcast MainMenu;
+	MainMenu.TimedMessage("Hello World", 10, 3000, true);
+	MainMenu.TimedMessage("Hello World", 5, 3000, false);
+	MainMenu.Divider(8);
+	MainMenu.Price("5.99");
 }
 
 int main() {
+	MainMenu();
+	Drinks Tea;
+	Tea.itemID = 1;
+	Tea.itemPrice = 5.99;
+	Tea.itemName = "English Tea";
 
-	bool debugMode = true;
-
-	DrinksMenu Coffee;
-	Coffee.itemID = 4;
+	Drinks Coffee;
+	Coffee.itemID = 2;
 	Coffee.itemPrice = 2.99;
 	Coffee.itemName = "Regular Coffee";
 
-	DrinksMenu Tea;
-	Tea.itemID = 1;
-	Tea.itemPrice = 5.99;
-	Tea.itemName = "Tea";
-
-	Broadcast(Tea.itemName);
+	Drinks HotChoc;
+	HotChoc.itemID = 3;
+	HotChoc.itemPrice = 3.99;
+	HotChoc.itemName = "Hot Chocolate";
 
 	return 0;
 }
